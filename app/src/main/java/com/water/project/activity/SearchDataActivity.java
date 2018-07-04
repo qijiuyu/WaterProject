adf4ca7a50ec1af7e3ea495f25618a0ad9758983
qijiuyu79 <jiuyu.qi@edaibu.net>
2018-07-04 15:49:52 +0800

    添加关于我们页面

--- app/src/main/java/com/water/project/activity/SearchDataActivity.java
+++ app/src/main/java/com/water/project/activity/SearchDataActivity.java
@@ -5,44 +5,42 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.water.project.R;
 
 /**
- * 首页
- * Created by Administrator on 2018/7/2 0002.
+ * 设置参数2
+ * Created by Administrator on 2018/7/4 0004.
  */
 
-public class MainActivity extends BaseActivity implements View.OnClickListener{
+public class SearchDataActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_getdata);
         initView();
     }
 
 
+    /**
+     * 初始化控件
+     */
     private void initView(){
-        findViewById(R.id.tv_setting).setOnClickListener(this);
-        findViewById(R.id.tv_about).setOnClickListener(this);
-        findViewById(R.id.tv_search).setOnClickListener(this);
+        TextView textView=(TextView)findViewById(R.id.tv_head);
+        textView.setText("数据查询");
+        findViewById(R.id.lin_back).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.tv_setting:
-                 setClass(SettingOneActivity.class);
-                 break;
-            case R.id.tv_search:
-                 setClass(SearchDataActivity.class);
-                 break;
-            case R.id.tv_about:
-                setClass(AboutActivity.class);
-                 break;
+            case R.id.lin_back:
+                finish();
+                break;
             default:
                 break;
         }
