2b414626697d2d2e42a947ccc52cf85a70d84e23
齐九玉 <2547863721@qq.com>
2020-09-09 09:24:04 +0800

    修改

--- app/src/main/java/com/water/project/activity/menu6/RedBDSignalActivity.java
+++ app/src/main/java/com/water/project/activity/menu6/RedBDSignalActivity.java
@@ -6,115 +6,79 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.view.Window;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
 import com.water.project.R;
 import com.water.project.activity.BaseActivity;
 import com.water.project.adapter.BAdapter;
 import com.water.project.application.MyApplication;
 import com.water.project.bean.BindService;
 import com.water.project.bean.Ble;
-import com.water.project.bean.eventbus.EventStatus;
-import com.water.project.bean.eventbus.EventType;
 import com.water.project.presenter.SendDataPersenter;
 import com.water.project.service.BleService;
 import com.water.project.utils.DialogUtils;
 import com.water.project.utils.SPUtil;
-import com.water.project.utils.ToastUtil;
 import com.water.project.utils.ble.BleContant;
 import com.water.project.utils.ble.BleObject;
 import com.water.project.utils.ble.SendBleStr;
 import com.water.project.view.DialogView;
 import com.water.project.view.MeasureListView;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
- * 发送数据
- * Created by Administrator on 2019/11/15.
+ * 读取北斗卫星通讯信号强度
  */
-
-public class BActivity extends BaseActivity {
+public class RedBDSignalActivity extends BaseActivity {
 
     @BindView(R.id.tv_head)
     TextView tvHead;
-    @BindView(R.id.tv_send)
-    TextView tvSend;
     @BindView(R.id.listView)
     MeasureListView listView;
-    @BindView(R.id.tv_dian_ya)
-    TextView tvDianYa;
-    @BindView(R.id.tv_list)
-    TextView tvList;
-    @BindView(R.id.scrollView)
-    ScrollView scrollView;
     //MVP对象
     private SendDataPersenter sendDataPersenter;
     //下发命令的编号
     private int SEND_STATUS;
-    //计数器
-    private Timer mTimer;
-    private int time = 0;
-    private Handler handler=new Handler();
     private DialogView dialogView;
+
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_b);
+        setContentView(R.layout.activity_red_bd_signal);
         ButterKnife.bind(this);
         initView();
         //注册广播
         register();
+        sendData(BleContant.RED_BEI_DOU_XIN_HAO_QIANG_DU);
     }
 
     /**
      * 初始化
      */
-    private void initView() {
-        //注册EventBus
-        EventBus.getDefault().register(this);
+    private void initView(){
         //实例化MVP
         sendDataPersenter = new SendDataPersenter(this);
-        tvHead.setText("北斗通讯发送实时数据");
+        tvHead.setText("读取北斗卫星通讯信号强度");
     }
 
-
-    @OnClick({R.id.lin_back, R.id.tv_send, R.id.tv_send2})
+    @OnClick({R.id.lin_back, R.id.tv_send})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.lin_back:
-                finish();
+                 finish();
                 break;
-            //让设备通过北斗方式发送实时数据
             case R.id.tv_send:
-                if(time>0){
-                    ToastUtil.showLong("请等待60秒后再进行操作");
-                    return;
-                }
-                sendData(BleContant.BEI_DOU_FANG_SHI_SEND_DATA);
+                sendData(BleContant.RED_BEI_DOU_XIN_HAO_QIANG_DU);
                 break;
-            case R.id.tv_send2:
-                 setClass(RedBDSignalActivity.class);
-                 break;
             default:
                 break;
         }
     }
-
 
 
     /**
@@ -137,16 +101,12 @@ public class BActivity extends BaseActivity {
             //扫描并重连蓝牙
             final Ble ble = (Ble) MyApplication.spUtil.getObject(SPUtil.BLE_DEVICE, Ble.class);
             if (null != ble) {
-                DialogUtils.showProgress(BActivity.this, "扫描并连接蓝牙设备...");
+                DialogUtils.showProgress(RedBDSignalActivity.this, "扫描并连接蓝牙设备...");
                 bleService.scanDevice(ble.getBleName());
             }
             return;
         }
-        if(SEND_STATUS==BleContant.BEI_DOU_FANG_SHI_SEND_DATA){
-            DialogUtils.showProgress(BActivity.this, "正在发送数据,请稍候...");
-        }else{
-            DialogUtils.showProgress(BActivity.this, "正在读取信号强度,请稍候...");
-        }
+        DialogUtils.showProgress(RedBDSignalActivity.this, "正在读取信号强度,请稍候...");
         SendBleStr.sendBleData(this,SEND_STATUS);
     }
 
@@ -192,43 +152,22 @@ public class BActivity extends BaseActivity {
                     String data = intent.getStringExtra(BleService.ACTION_EXTRA_DATA);
 
                     try {
-                        //延时60秒后
-                        if(SEND_STATUS==BleContant.BEI_DOU_FANG_SHI_SEND_DATA){
-                            startTime();
-                        }
 
                         if(data.endsWith(">ERR")){
-                            dialogView = new DialogView(dialogView,BActivity.this, "北斗通讯部分出现故障，请联系维护人员!","好的", null, null, null);
+                            dialogView = new DialogView(dialogView,activity, "北斗通讯部分出现故障，请联系维护人员!","好的", null, null, null);
                             dialogView.show();
                         }else{
                             data=data.replace("GDBDSQ","").replace(">OK", "");
                             //显示信号列表
-                            tvList.setVisibility(View.VISIBLE);
                             String[] strs=data.split(",");
-                            BAdapter bAdapter=new BAdapter(BActivity.this,strs);
+                            BAdapter bAdapter=new BAdapter(activity,strs);
                             listView.setAdapter(bAdapter);
-                            //显示电压值
-
-                            String strDy=strs[strs.length-1];
-                            if(strDy.indexOf("V")!=-1){
-                                String[] dianya=strDy.split("V");
-                                tvDianYa.setText("发送数据成功，北斗通讯部分电压值："+dianya[1]+"V\n请联系接收中心查看数据");
-                            }
-                            handler.postDelayed(new Runnable() {
-                                public void run() {
-                                    scrollView.scrollTo(0,0);
-                                }
-                            },500);
                         }
                     }catch (Exception e){
                         e.printStackTrace();
                     }
                     break;
                 case BleService.ACTION_INTERACTION_TIMEOUT:
-                    if(SEND_STATUS==BleContant.BEI_DOU_FANG_SHI_SEND_DATA){
-                        //动态改变秒数
-                        startTime();
-                    }
                     sendDataPersenter.timeOut(SEND_STATUS);
                     break;
                 case BleService.ACTION_SEND_DATA_FAIL:
@@ -245,71 +184,10 @@ public class BActivity extends BaseActivity {
     };
 
 
-    /**
-     * EventBus注解
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    @Subscribe
-    public void onEvent(EventType eventType) {
-        switch (eventType.getStatus()) {
-            //发送命令
-            case EventStatus.SEND_CHECK_MCD:
-                final int cmd = (int) eventType.getObject();
-                if(SEND_STATUS==BleContant.BEI_DOU_FANG_SHI_SEND_DATA && time>0){
-                    ToastUtil.showLong("请等待60秒后再进行操作");
-                    return;
-                }
-                sendData(cmd);
-                break;
-            default:
-                break;
-        }
-    }
-
-
-    /**
-     * 动态改变验证码秒数
-     */
-    private void startTime() {
-        time=60;
-        //保存计时时间
-        SPUtil.getInstance(this).addString("new_time", String.valueOf((System.currentTimeMillis() + 60000)));
-        mTimer = new Timer();
-        mTimer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                if (time <= 0) {
-                    handler.post(new Runnable() {
-                        public void run() {
-                            mTimer.cancel();
-                            tvSend.setText("让设备通过北斗方式发送实时数据");
-                            SPUtil.getInstance(BActivity.this).removeMessage("new_time");
-                            tvSend.setBackgroundColor(getResources().getColor(R.color.color_1fc37f));
-                        }
-                    });
-                } else {
-                    --time;
-                    handler.post(new Runnable() {
-                        public void run() {
-                            tvSend.setText("让设备通过北斗方式发送实时数据("+time+")");
-                            tvSend.setBackgroundColor(getResources().getColor(R.color.color_DADADA));
-                        }
-                    });
-                }
-            }
-        }, 0, 1000);
-    }
-
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        EventBus.getDefault().unregister(this);
         unregisterReceiver(mBroadcastReceiver);
-        if(mTimer!=null){
-            mTimer.cancel();
-            mTimer.purge();
-            mTimer=null;
-        }
     }
 
 }
